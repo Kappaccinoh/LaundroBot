18251f70cc1095bca883d2b4fab9d223b1daf474
wui-hong <khoowuihong@yahoo.com.sg>
2024-01-19 21:46:44 +0800

    Revert bot back to original time left display

--- LaundroBot/laundrobot-telegram-main/bot.py
+++ LaundroBot/laundrobot-telegram-main/bot.py
@@ -8,8 +8,8 @@ from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 import asyncio
 #api_url = 'REDACTED'
-api_url = 'http://localhost:3002'
-online_token = 'REDACTED'
+api_url = 'https://free-api-ryfe.onrender.com'
+online_token = '6251328058:AAGlKyxiO77DDQ93xvKh_11iWwF2pemsVnE'
 testing_token = ''
 
 
@@ -20,10 +20,10 @@ logging.basicConfig(
 
 def returnMinutesLeft(updatedAt, timeLeftUserInput):
     #below is the code to do the simple boolean on/off
-    if timeLeftUserInput == 45:
-        return f"  IN USE  "
-    else:
-        return "NOT IN USE"
+    # if timeLeftUserInput == 45:
+    #     return f"  IN USE  "
+    # else:
+    #     return "NOT IN USE"
 
     #above is the code to do the simple boolean on/off
     yymmdd = updatedAt[0:10]
@@ -452,7 +452,7 @@ async def update_status_message(context: ContextTypes.DEFAULT_TYPE) -> None:
     await context.bot.edit_message_text( final_str, chat_id='-1001932990612', message_id='14', parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(testing_token).build()
+    application = ApplicationBuilder().token(online_token).build()
 
     
     start_handler = CommandHandler('start', start)
--- LaundroBot/laundrobotAPI-main/server.js
+++ LaundroBot/laundrobotAPI-main/server.js
@@ -10,7 +10,7 @@ app.use(cors())
 
 app.use(express.json())
 
-mongoose.connect('REDACTED')
+mongoose.connect('mongodb+srv://admin:admin@laundrobotapi.msler7u.mongodb.net/?retryWrites=true&w=majority')
 .then(() => {
     app.listen(3002, () => {
         console.log(`Node API app is running on port 3002`)
